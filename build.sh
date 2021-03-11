11c6e28739bb688d64b61180a6b39d9b32bea417
Mohammed Rafi Shaik <mshaik@xilinx.com>
2021-03-11 07:40:46 -0700

    reverting back to ea2 release

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ conf=${2:-Release}
 
 unset LD_LIBRARY_PATH;
 source ${sdkdir}/environment-setup-aarch64-xilinx-linux;
+cp ./script/lib* ${sdkdir}/sysroots/aarch64-xilinx-linux/usr/lib/
 mkdir -p build
 cd build
 cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=${conf} -DCMAKE_TOOLCHAIN_FILE=${sdkdir}/sysroots/x86_64-petalinux-linux/usr/share/cmake/OEToolchainConfig.cmake ../ && make -j && make package
