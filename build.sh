34944d99abc093c69d3244ddde75b1ca7a7e175b
Mohammed Rafi Shaik <mshaik@xilinx.com>
2021-03-11 02:35:37 -0700

    libCMSIS and Opencv included in cmake

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ source ${sdkdir}/environment-setup-aarch64-xilinx-linux;
 cp ./script/lib* ${sdkdir}/sysroots/aarch64-xilinx-linux/usr/lib/
 mkdir -p build
 cd build
-cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=${conf} -DCMAKE_TOOLCHAIN_FILE=${sdkdir}/sysroots/x86_64-petalinux-linux/usr/share/cmake/OEToolchainConfig.cmake ../ && make -j && make package
+cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DENABLE_PRECOMPILED_HEADERS=OFF -D CMAKE_NO_SYSTEM_FROM_IMPORTED=ON -DCMAKE_BUILD_TYPE=${conf} -DCMAKE_TOOLCHAIN_FILE=${sdkdir}/sysroots/x86_64-petalinux-linux/usr/share/cmake/OEToolchainConfig.cmake ../ && make -j && make package
 cd ..
